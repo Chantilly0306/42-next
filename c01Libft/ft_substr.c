a4cdcdf599f2d7bc7368b08ecc228ca460a629a3
hsilin <hsilin@f0r11s1.paris.42.school>
2026-06-10 20:57:15 +0200

    strsub

--- c01Libft/ft_substr.c
+++ c01Libft/ft_substr.c
@@ -2,5 +2,34 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	char	*sub;
+	size_t	i;
+	size_t	s_len;
 
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		len = 0;
+	else if (len > s_len - start)
+		len = s_len - start;
+	sub = malloc(sizeof(char) * (len + 1));
+	if (!sub)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
 }
+/*
+int	main(void)
+{
+	char	*sub = ft_substr("Hello World", 6, 5);
+
+	printf("%s\n", sub);
+}
+*/
